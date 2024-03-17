e24f85af2f7d644cbfd4b03a29f3a7a722353339
tiany <tiany@uncw.edu>
2024-03-16 22:15:17 -0400

    test

---
+++ web/lswg/about.php
@@ -0,0 +1,51 @@
+<?php 
+include 'header.html'; 
+?>
+
+<h3>  Project Team </h3>
+
+<table width=100%> <tr> <td> 
+Dr. Yudong Tian </td><td>PI </td> <td>  yudong.tian&#0064;nasa.gov </td> 
+ </tr> 
+<tr> <td>
+Dr. Christa Peters-Lidard </td><td>Co-I </td><td> christa.d.peters-lidard&#000064;nasa.gov </td> </tr> 
+<tr> <td>
+Dr. Ling Tang </td> <td> Research Associate </td> <td> ling.tang&#0064;nasa.gov</td> 
+</tr> 
+<tr> <td> Ms. Rebekah Esmaili  </td> 
+<td> Grad. Student Res. Assistant</td> 
+<td> resmaili&#00064;atmos.umd.edu </td> </tr>
+<tr><td colspan=3> <br> <h3> Program Management </h3> </td> </tr> 
+<tr> <td> 
+Ms. Martha E. Maiden</td>
+<td>NASA HQ </td><td>202-358-1078 </td> </tr> </table>
+<br> 
+
+<h3>Past and Present Collaborators and Co-authors:</h3> 
+Robert Adler<br> 
+Bhaskar Choudhury<br>
+John Eylander <br>
+Matt Garcia <br> 
+Abebe Gebregiorgis <br>
+James Geiger<br> 
+Emad Habib<br> 
+Yang Hong<br> 
+Paul Houser<br> 
+Faisal Hossain<br> 
+Kuo-lin Hsu <br>
+George Huffman<br> 
+Robert Joyce<br> 
+Takuji Kubota<br>
+Sujay Kumar<br> 
+Xin Lin<br> 
+Hongbo Su<br> 
+F. Joe Turk<br> 
+Tomoo Ushio<br> 
+Huan Wu<br> 
+Pingping Xie<br> 
+Koray Yilmaz<br>
+Jing Zeng<br> 
+
+<?php
+include 'footer.html'; 
+?>
