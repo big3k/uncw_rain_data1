195453bd7a6ee43427a86ed1075b69ddfcf0d24a
tiany <tiany@uncw.edu>
2024-04-26 12:12:03 -0400

    Sample files to show how to compile HDF5 reader with gfortran.

---
+++ tiany/Regrid/compile.sh
@@ -0,0 +1,6 @@
+
+h5fc -o reproj reproj.F90
+
+#[tiany@rain Regrid]$ h5fc -show
+#gfortran -O2 -g -pipe -Wall -Werror=format-security -Wp,-D_FORTIFY_SOURCE=2 -Wp,-D_GLIBCXX_ASSERTIONS -fexceptions -fstack-protector-strong -grecord-gcc-switches -specs=/usr/lib/rpm/redhat/redhat-hardened-cc1 -specs=/usr/lib/rpm/redhat/redhat-annobin-cc1 -m64 -mtune=generic -fasynchronous-unwind-tables -fstack-clash-protection -fcf-protection -I/usr/lib64/gfortran/modules -I/usr/include -L/usr/lib64 -lhdf5hl_fortran -lhdf5_hl -lhdf5_fortran -lhdf5 -Wl,-z,relro -Wl,-z,now -specs=/usr/lib/rpm/redhat/redhat-hardened-ld -fPIC -Wl,-z,now -Wl,--as-needed -lsz -lz -ldl -lm -Wl,-rpath -Wl,/usr/lib64
+
