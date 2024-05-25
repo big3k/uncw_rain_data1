d6e8a3457d5b80c3d5ea1688f66be44f08ebd918
tiany <tiany@uncw.edu>
2024-05-25 11:43:09 -0400

    Sample script to download TRPOPICS data.

---
+++ tiany/TROPICS/get_data.sh
@@ -0,0 +1,4 @@
+
+wget --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --keep-session-cookies -r -c -nH -nd -np -A nc --content-disposition "https://disc2.gesdisc.eosdis.nasa.gov/data/TROPICS_L2B/TROPICS03PRPSL2B.0.2/2024/001/"
+
+#wget --load-cookies ~/.urs_cookies --save-cookies ~/.urs_cookies --keep-session-cookies --content-disposition "https://disc2.gesdisc.eosdis.nasa.gov/data/TROPICS_L2B/TROPICS03MIRSL2B.0.2/2024/001/TROPICS03.MIRS.L2B.Orbit03332.V02-06.ST20240101-012226.ET20240101-025732.CT20240312-005225.nc
