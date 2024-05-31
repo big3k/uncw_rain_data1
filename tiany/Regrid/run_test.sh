a27ed519670671cdaa110008b02c0cb64243cc6c
tiany <tiany@uncw.edu>
2024-05-31 17:17:59 -0400

    Added web pages and revised run_test.sh to trim plots.

--- tiany/Regrid/run_test.sh
+++ tiany/Regrid/run_test.sh
@@ -8,7 +8,16 @@ set gxout grfill
 d sti
 cbarn 
 printim grid_sti.png png white x1400 y1000
+c
+set gxout grfill
+set grads off
+set clevs 0.1 0.2 0.4 0.8 1.6 3.2 6.4 12.8 25.6
+d sfcrain
+cbarn 
+printim grid_sfcrain.png png white x1400 y1000
 quit
 EOF
 
+ convert -trim grid_sti.png trim_grid_sti.png
+ convert -trim grid_sfcrain.png trim_grid_sfcrain.png
 
