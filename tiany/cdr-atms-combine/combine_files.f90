d5eb6a5940ee64fb2542d50e5d7f83bd93ae6825
tiany <tiany@uncw.edu>
2024-09-18 17:00:48 -0400

    fixed nf90_def_var sequence issue: all variables need to be defined first and call nf90_enddef before writting to files.

--- tiany/cdr-atms-combine/combine_files.f90
+++ tiany/cdr-atms-combine/combine_files.f90
@@ -22,19 +22,19 @@
       integer :: ncid, status
       integer :: d2dims(2), d3dims(3) 
       INTEGER(KIND=4) :: x_dimid, y_dimid, z_dimid
-      INTEGER(KIND=4) :: at_varid, lon_varid, lat_varid 
+      INTEGER(KIND=4) :: at_varid, lon_varid, lat_varid, sza_varid 
 
-      character(len=6) :: stime  ! start time string of combined file 
-      character(len=6) :: etime  ! end time string of combined file 
+      character(len=9) :: dtime  ! d20120302
+      character(len=8) :: stime  ! start time string of combined file, t2034286 
+      character(len=8) :: etime  ! end time string of combined file , e2217243
  
+      dtime=gatmo_files_in_bin(1)(11:19) 
       stime=gatmo_files_in_bin(1)(21:28) 
       etime=gatmo_files_in_bin(nf_in_bin)(30:37) 
       ! construct output file name
       ! e.g., COMBO_npp_d20120302_t2034286_e2217243_b01794_c20191106092131140068_ADu_dev.nc
-      outf=gatmo_files_in_bin(1)
-      outf(30:37)=gatmo_files_in_bin(nf_in_bin)(30:37)
-      outf(1:5)="COMBO"
-      outf(76:77)="nc"
+      ! NPP_ATMS_d20120302_t1708393_e1851323.nc
+      outf="NPP_ATMS_"//dtime//"_"//stime//"_"//etime//".nc" 
       write(*, *) trim(outf) 
       
       !open netcdf for saving variables 
@@ -50,11 +50,22 @@
       status=nf90_def_dim(ncid, "nfovs", ny, y_dimid)
       status=nf90_def_dim(ncid, "nscans", nz, z_dimid)
       d3dims=(/ x_dimid, y_dimid, z_dimid /)
+      d2dims=(/ y_dimid, z_dimid /)
+
+      ! Define all the variables 
+      status=nf90_def_var(ncid, "AntennaTemperature", &
+             NF90_FLOAT, d3dims, at_varid, deflate_level=6)
+      status=nf90_def_var(ncid, "Longitude", &
+             NF90_FLOAT, d2dims, lon_varid, deflate_level=6)
+      status=nf90_def_var(ncid, "Latitude", &
+             NF90_FLOAT, d2dims, lat_varid, deflate_level=6)
+      status=nf90_def_var(ncid, "SatelliteZenithAngle", &
+             NF90_FLOAT, d2dims, sza_varid, deflate_level=6)
+      status=nf90_enddef(ncid)
+
       allocate(d3data(nx, ny, nz)) 
       d3data = data_buff(1:nx, 1:ny, 1:nz) 
       !status=nf90_def_var(ncid, "/All_Data/ATMS-TDR_All/AntennaTemperature", &
-      status=nf90_def_var(ncid, "AntennaTemperature", &
-             NF90_FLOAT, d3dims, at_varid, deflate_level=6)
 
       !status=nf90_enddef(ncid)
       status=nf90_put_var(ncid, at_varid, d3data)
@@ -65,40 +76,44 @@
             gatmo_files_in_bin, nf_in_bin, data_buff, nx, ny, nz)
       ! scale to be added
 
-      d2dims=(/ y_dimid, z_dimid /)
       allocate(d2data(nx, ny))
       d2data = data_buff(1:nx, 1:ny, 1)
-      status=nf90_def_var(ncid, "Longitude", &
-             NF90_FLOAT, d2dims, lon_varid, deflate_level=6)
 
-      !status=nf90_enddef(ncid)
       status=nf90_put_var(ncid, lon_varid, d2data)
       deallocate(d2data)
-
 
      !get 2D data
       call merge_data("/All_Data/ATMS-SDR-GEO_All/Latitude", gatmo_dir, &
             gatmo_files_in_bin, nf_in_bin, data_buff, nx, ny, nz)
       ! scale to be added
 
-      d2dims=(/ y_dimid, z_dimid /)
       allocate(d2data(nx, ny))
       d2data = data_buff(1:nx, 1:ny, 1)
-      status=nf90_def_var(ncid, "Latitude", &
-             NF90_FLOAT, d2dims, lat_varid, deflate_level=6)
 
-      status=nf90_enddef(ncid)
+      !status=nf90_enddef(ncid)
       status=nf90_put_var(ncid, lat_varid, d2data)
       deallocate(d2data)
 
-      !call merge_data("/All_Data/ATMS-SDR-GEO_All/Longitude", gatmo_dir, & 
-      !      gatmo_files_in_bin, nf_in_bin, data_buff, nx, ny, nz)  
+     !get 2D data
+      call merge_data("/All_Data/ATMS-SDR-GEO_All/SatelliteZenithAngle", gatmo_dir, &
+            gatmo_files_in_bin, nf_in_bin, data_buff, nx, ny, nz)
+      ! scale to be added
 
+      allocate(d2data(nx, ny))
+      d2data = data_buff(1:nx, 1:ny, 1)
 
-      write(*, *)"/All_Data/ATMS-TDR_All/AntennaTemperature", nx, ny, nz 
-      !call save_to_nc("/All_Data/ATMS-SDR-GEO_All/Longitude", data_buff, nx, ny, nz)
+      status=nf90_put_var(ncid, sza_varid, d2data)
+      deallocate(d2data)
+100   continue
  
-       ! stuff to big arrary
+      ! Get "/All_Data/ATMS-TDR_All/BeamTime" from TATMS files
+      ! However, it has "H5T_STD_I64LE" type, which won't fit in netcdf
+      !          DATASET "BeamTime" {
+      !      DATATYPE  H5T_STD_I64LE
+      !      DATASPACE  SIMPLE { ( 12, 96 ) / ( H5S_UNLIMITED, H5S_UNLIMITED ) }
+
+      !Note that there are no netCDF types corresponding to 64-bit integers or 
+      ! to characters wider than 8 bits in the current version of the netCDF library.
        
       status=nf90_close(ncid)
       return 
