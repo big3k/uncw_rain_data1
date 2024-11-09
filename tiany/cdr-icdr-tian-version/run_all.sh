160b273791b813fd85cb8a399b317bf8e8f85d88
tiany <tiany@uncw.edu>
2024-11-09 15:24:16 -0500

    Added progress message.

--- tiany/cdr-icdr-tian-version/run_all.sh
+++ tiany/cdr-icdr-tian-version/run_all.sh
@@ -8,14 +8,19 @@ mkdir -p $top_dir/logs
 run_date=`date +%FT%H-%M-%S`
 
 # Pre-process GFS data 
+echo "Exacting GFS data ..."
 ./code/extract_gfs.sh  > $top_dir/logs/fcdr_amsua_${run_date}.log 2>&1
 
 # FCDR and TCDR. 
 # For each satellite, amsua has to be done first as amsub/mhs depends on it 
 for sat in $sats; do 
+ echo "Processing $sat amsua fcdr ..."
  ./code/proc_fcdr.sh amsua $sat  > $top_dir/logs/fcdr_amsua_${run_date}.log 2>&1
+ echo "Processing $sat amsua tcdr ..."
  ./code/proc_tcdr.sh amsua $sat  > $top_dir/logs/tcdr_amsua_${run_date}.log 2>&1
+ echo "Processing $sat mhs fcdr ..."
  ./code/proc_fcdr.sh mhs $sat  > $top_dir/logs/fcdr_mhs_${run_date}.log 2>&1
+ echo "Processing $sat mhs tcdr ..."
  ./code/proc_tcdr.sh mhs $sat  > $top_dir/logs/ccdr_mhs_${run_date}.log 2>&1
 done
 
