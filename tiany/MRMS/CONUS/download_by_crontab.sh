8165ac5384fdb9cc1d9f58c77ab6e99949c8860d
tiany <tiany@uncw.edu>
2026-05-24 22:25:13 -0400

    Fixed cron script.

--- tiany/MRMS/CONUS/download_by_crontab.sh
+++ tiany/MRMS/CONUS/download_by_crontab.sh
@@ -1,8 +1,12 @@
-#somehow crontab did not run. so do it here
-s=20260508
-#e=20250131
-e=20260511
+# Run by cron every day, download 2-day's data: s to s+1   
+#  notwithstanding e=s
 
+s=$(date -d "-12 days" +%Y%m%d) 
+e=$s 
+#s=20260208 
+#e=20250131
+
+echo Downloading $s $e
 /data1/tiany/MRMS/CONUS/download_PrecipRate.sh $s $e > /data1/tiany/MRMS/CONUS/log.${s}_$e.download_PrecipRate.txt 2>&1 
 /data1/tiany/MRMS/CONUS/download_PrecipFlag.sh $s $e > /data1/tiany/MRMS/CONUS/log.${s}_$e.download_PrecipFlag.txt 2>&1 
 /data1/tiany/MRMS/CONUS/download_RadarQualityIndex.sh $s $e > /data1/tiany/MRMS/CONUS/log.${s}_$e.download_RadarQualityIndex.txt 2>&1 
